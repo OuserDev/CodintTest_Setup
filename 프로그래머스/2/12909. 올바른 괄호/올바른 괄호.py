206fa1f09d667efdacf34dcfbf4b53474a03a0a3
몰타 섬의 개 <141126078+OuserDev@users.noreply.github.com>
2025-04-06 04:52:54 +0900

    [level 2] Title: 올바른 괄호, Time: 7.56 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,20 +1,12 @@
-def solution(s):
-    #패착 01. 이 if문을 for문밖에서 확인하려고 함
-    if not s or s[0] == ')' or s[-1] == '(':
-        return False
 
-    cnt = 0
+def solution(s):
+    stack = []
     for i in s:
         if i == '(':
-            cnt += 1
-        #패착 02. if i == ')' and cnt >= 1:로 해버리면, 닫괄이 열괄보다 먼저 나오면 처리가 없음
-        elif i == ')':
-            if cnt >= 1:
-                cnt -= 1
-            else: #만약, ())같은 놈이 나온 경우:
+            stack.append(i)
+        if i == ')':
+            try:
+                stack.pop()
+            except IndexError:
                 return False
-                
-    if cnt == 0:
-        return True
-    else:
-        return False
+    return len(stack) == 0
